f00f0d2170ec924bc8faf26966e756f0a4e1387c
kevcodes <131954190+Kelv11@users.noreply.github.com>
2025-06-03 17:30:07 +0300

    Update landing.tsx

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Card from "@/components/card";
+import Card from "@/components/Card";
 import Button from "@/components/Button";
 
 const Landing: React.FC = () => {
